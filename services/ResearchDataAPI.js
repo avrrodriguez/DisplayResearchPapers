4123ced5360956018323a404b84d7cefcb8e514d
Abraham V Rodriguez <pqavrr835@gmail.com>
2025-08-23 17:34:37 -0400

    changes which api used, makes the api string dynamic

--- services/ResearchDataAPI.js
+++ services/ResearchDataAPI.js
@@ -1,6 +1,10 @@
 export default async function ResearchDataAPI() {
+  let query = "covid+vaccination";
+  let fields = "title,year,authors,abstract,s2FieldsOfStudy,journal,publicationDate,url,openAccessPdf";
+  let limit = 3;
+  let offset = 0;
   const response = await fetch(
-    "https://api.semanticscholar.org/graph/v1/paper/search/bulk?query=covid+vaccination&fields=title,year,authors,abstract,s2FieldsOfStudy,journal,publicationDate,url,openAccessPdf"
+    `https://api.semanticscholar.org/graph/v1/paper/searchquery=${query}&fields=${fields}&offset=${offset}&limit=${limit}`
   );
   return await response.json();
 }
